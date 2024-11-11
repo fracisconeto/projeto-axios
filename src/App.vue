b9a8641cfbe9228230ff77360e71fb9e3e7e37c5
fracisconeto <vargasfrancisco227@gmail.com>
2024-11-11 16:54:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+// import axios from 'axios';
+
+</script>
+
+<template>
+  Chave da API
+4c9bdb0b51dd5ec3169b8f38c825e915
+Token de Leitura da API
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0YzliZGIwYjUxZGQ1ZWMzMTY5YjhmMzhjODI1ZTkxNSIsIm5iZiI6MTczMTM1NDc4OS4wMzI0MTU0LCJzdWIiOiI2NzMyNWQ1ODYxNjI2YWMxMDZiZTg3MWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.te2CtfZz2gZvROkRJ6T7TbyU7BCud4Fn8N8ZZXbrPJk
+
+</template>
+
+<style scoped>
+
+</style>
